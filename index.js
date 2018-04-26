7a75d7c943db05b70e36530d53d43815edc53805
leonardom249 <leonardo.megan@gmail.com>
2018-04-26 12:43:25 -0500

    added error handling middlewear to index.js

--- index.js
+++ index.js
@@ -39,6 +39,20 @@ app.use('/api', authRouter);
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
 
 function runServer(port = PORT) {
   const server = app
